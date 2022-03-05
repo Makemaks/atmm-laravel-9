9e2bb52306b405a2d947fd89c3a62a1271825bb6
James Bolongan <jamesbolongan@Jamess-MacBook-Air.local>
2022-03-05 20:30:52 +0800

    nmi, passport, google2fa settings

---
+++ config/nmi.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * NMI Payments
+ * https://www.nmi.com/
+ */
+
+return [
+    'nmi_api_key' => env('NMI_API_KEY', ''),
+    'base_url' => env('BASE_URL', 'https://allthingsmichaelmclean.com/'),
+];
